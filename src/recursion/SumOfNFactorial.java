264b00a1498b01a68855651ec569edf87d533df9
bhushantiturkar2001 <titurkarbhushan@gmail.com>
2026-02-23 19:42:46 +0530

    Factorial of n using functional way

---
+++ src/recursion/SumOfNFactorial.java
@@ -0,0 +1,18 @@
+package recursion;
+
+import java.util.Scanner;
+
+public class SumOfNFactorial {
+
+    static int fact(int n){
+        if (n==1) return 1;
+        else return n * fact(n-1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Input: ");
+        int n = sc.nextInt();
+        System.out.println(fact(n));
+    }
+}
