96d697944a309f8a8f8a28b5e6dddb1563c7e34e
bhushantiturkar2001 <titurkarbhushan@gmail.com>
2026-02-23 19:37:33 +0530

    Sum of n using functional way

---
+++ src/recursion/SumOFNFunctionalWay.java
@@ -0,0 +1,20 @@
+package recursion;
+
+import java.util.Scanner;
+
+public class SumOFNFunctionalWay {
+
+    static int sum(int n){
+        if (n==0)return 0;
+        else return (n + sum(n-1));
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Input: ");
+        int n = sc.nextInt();
+        System.out.println(sum(n));
+
+    }
+}
